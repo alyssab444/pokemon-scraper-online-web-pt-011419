153b54124577363b7efcfad2a21c9cbf10a6078a
Alyssa Brown <alyssa.brown@live.com>
2019-03-30 03:42:53 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,7 @@ class Pokemon
     Pokemon.new(id: found_pokemon[0], name: found_pokemon[1], type: found_pokemon[2], db: db)
   end
   
-  def alter_hp(id, db)
+  def alter_hp(new_hp, db)
     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", , [self.id])
     self.hp = num
   end
